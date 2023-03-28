1ac95c0f78b7c72573d05244c3a3a87bc2355fde
Jack Roudebush <roudebushjack@gmail.com>
2023-03-27 22:10:33 -0400

    Add event listeners for hover

--- script.js
+++ script.js
@@ -4,5 +4,12 @@ for (let i = 0; i < 256; i++) {
     const grid = document.createElement('div');
     grid.classList.add('blankItem');
     canvas.appendChild(grid);
+    grid.addEventListener('mouseover', () => {
+        grid.classList.add('hover');
+    })
+    grid.addEventListener('mouseout', () => {
+        grid.classList.remove('hover');
+    })
 }
+
 
