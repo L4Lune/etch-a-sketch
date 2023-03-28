c17077add16e43a6c9f6f16500b80aaa0d1caffa
Jack Roudebush <roudebushjack@gmail.com>
2023-03-28 15:29:39 -0400

    Remove old for loop and remove button logic till I have time to think about it

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
 const canvas = document.querySelector('#canvas');
-
-const userChoice = 16;
-const gridSelection = document.querySelector('#gridSelection');
-gridSelection.addEventListener('click', () => {
-    const userChoice = Number(prompt("How many squares per side you looking for?"));  
-})
-
-// const userChoice = Number(prompt("How many squares per side you looking for?"));
-
-// for (let i = 0; i < 4; i++) {
-//     const grid = document.createElement('div');
-//     grid.classList.add('blankItem');
-//     canvas.appendChild(grid);
-//     grid.addEventListener('mouseover', () => {
-//         grid.classList.add('hover');
-//     })
-//     grid.addEventListener('mouseout', () => {
-//         grid.classList.remove('hover');
-//     })
-// }
+const userChoice = Number(prompt("How many squares per side you looking for?"));
 
 for (let i = 0; i < userChoice; i++) {
     const subCanvas = document.createElement('div');
