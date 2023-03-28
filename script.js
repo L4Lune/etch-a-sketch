b970ae8442bcd5a4946330a9d262a5abe032a97f
Jack Roudebush <roudebushjack@gmail.com>
2023-03-28 14:51:41 -0400

    Add for loop to create rowns and columns in the grid

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 const canvas = document.querySelector('#canvas');
 
-for (let i = 0; i < 256; i++) {
-    const grid = document.createElement('div');
-    grid.classList.add('blankItem');
-    canvas.appendChild(grid);
-    grid.addEventListener('mouseover', () => {
-        grid.classList.add('hover');
-    })
-    grid.addEventListener('mouseout', () => {
-        grid.classList.remove('hover');
-    })
+const userChoice = 2;
+
+// for (let i = 0; i < 4; i++) {
+//     const grid = document.createElement('div');
+//     grid.classList.add('blankItem');
+//     canvas.appendChild(grid);
+//     grid.addEventListener('mouseover', () => {
+//         grid.classList.add('hover');
+//     })
+//     grid.addEventListener('mouseout', () => {
+//         grid.classList.remove('hover');
+//     })
+// }
+
+for (let i = 0; i < userChoice; i++) {
+    const subCanvas = document.createElement('div');
+    subCanvas.classList.add('subCanvas');
+    canvas.appendChild(subCanvas);
+    for (let i = 0; i < userChoice; i++) {
+        const grid = document.createElement('div');
+        grid.classList.add('blankItem');
+        subCanvas.appendChild(grid);
+        grid.addEventListener('mouseover', () => {
+            grid.classList.add('hover');
+        })
+        grid.addEventListener('mouseout', () => {
+            grid.classList.remove('hover');
+        })
+    }
 }
 
 
