5f754f5e67901ffd6eb6e9eba06e7725f2474443
Jack Roudebush <roudebushjack@gmail.com>
2023-03-30 13:27:18 -0400

    Finish project

--- script.js
+++ script.js
@@ -3,33 +3,22 @@ let userChoice = 16;
 const gridSelection = document.querySelector('#gridSelection');
 const oldUserChoice = userChoice;
 
-
-// gridSelection.addEventListener('click', () => {
-//     removeGrid(oldUserChoice);
-// })
-
 gridSelection.addEventListener('click', function(e) {
     getUserInput();
     removeGrid();
     createGrid();
 })
 
-// gridSelection.addEventListener('click', () => {
-//     const userChoice = Number(prompt("How many squares per side you looking for?"));
-// })
-// gridSelection.addEventListener('click', () => {
-//     removeGrid();
-// })
-// gridSelection.addEventListener('click', () => {
-//     createGrid();
-// })
-
-
 function getUserInput() {
     userChoice = parseInt(prompt("How many squares per side you looking for?"));
-    return userChoice;
-    console.log(userChoice);
-}
+    if (userChoice < 100) {
+        return userChoice
+    } else {
+        userChoice = parseInt(prompt("Enter a number less than 100 please."))
+        userChoice = 16;
+    }
+
+} 
 function createGrid() {
     for (let i = 0; i < userChoice; i++) {
         const subCanvas = document.createElement('div');
@@ -45,27 +34,8 @@ function createGrid() {
             grid.addEventListener('mouseout', () => {
                 grid.classList.remove('hover');
             })
-        }
-    }
-}
-
-function recreateGrid() {
-    while (canvas.firstChild) {
-        canvas.removeChild(canvas.firstChild);
-    }
-    for (let i = 0; i < userChoice; i++) {
-        const subCanvas = document.createElement('div');
-        subCanvas.classList.add('subCanvas');
-        canvas.appendChild(subCanvas);
-        for (let i = 0; i < userChoice; i++) {
-            const grid = document.createElement('div');
-            grid.classList.add('blankItem');
-            subCanvas.appendChild(grid);
-            grid.addEventListener('mouseover', () => {
-                grid.classList.add('hover');
-            })
-            grid.addEventListener('mouseout', () => {
-                grid.classList.remove('hover');
+            grid.addEventListener('click', () => {
+                grid.classList.toggle("filled"); 
             })
         }
     }
@@ -91,6 +61,9 @@ for (let i = 0; i < userChoice; i++) {
         })
         grid.addEventListener('mouseout', () => {
             grid.classList.remove('hover');
+        })
+        grid.addEventListener('click', () => {
+            grid.classList.toggle("filled"); 
         })
     }
 }
