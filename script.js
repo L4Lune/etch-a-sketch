0928c664cc3b0ec9cee1dbca79af3576bd4c71eb
Jack Roudebush <roudebushjack@gmail.com>
2023-03-29 23:59:03 -0400

    Mess with functions to recreate the grid, but it isn't working

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const canvas = document.querySelector('#canvas');
-let userChoice = 16;
+const userChoice = 16;
 const gridSelection = document.querySelector('#gridSelection');
-const oldUserChoice = userChoice;
 
-gridSelection.addEventListener('click', () => {
-    removeGrid(oldUserChoice);
-})
+
+// gridSelection.addEventListener('click', () => {
+//     removeGrid(oldUserChoice);
+// })
 gridSelection.addEventListener('click', () => {
     let userChoice = Number(prompt("How many squares per side you looking for?"));
 })
 gridSelection.addEventListener('click', () => {
-    createGrid(userChoice);
+    recreateGrid(userChoice);
 })
 
 function createGrid(userChoice) {
@@ -33,59 +33,44 @@ function createGrid(userChoice) {
 }
 
 function recreateGrid(userChoice) {
-    const subCanvas = document.querySelector('.subCanvas')
-    for (let i = 0; i < oldUserChoice; i++) {
-        canvas.remove(subCanvas);
-        const grid = document.querySelector('.blankItem');
-        for (let i = 0; i < oldUserChoice; i++) {
-            subCanvas.remove(grid);
-        }
+    const removeSubcanvas = document.querySelector('.subCanvas');
+    canvas.removeChild(removeSubcanvas);
+    for (let i = 0; i < userChoice; i++) {
+        const subCanvas = document.createElement('div');
+        subCanvas.classList.add('subCanvas');
+        canvas.appendChild(subCanvas);
         for (let i = 0; i < userChoice; i++) {
-            const subCanvas = document.createElement('div');
-            subCanvas.classList.add('subCanvas');
-            canvas.appendChild(subCanvas);
-            for (let i = 0; i < userChoice; i++) {
-                const grid = document.createElement('div');
-                grid.classList.add('blankItem');
-                subCanvas.appendChild(grid);
-                grid.addEventListener('mouseover', () => {
-                    grid.classList.add('hover');
-                })
-                grid.addEventListener('mouseout', () => {
-                    grid.classList.remove('hover');
-                })
-            }
+            const grid = document.createElement('div');
+            grid.classList.add('blankItem');
+            subCanvas.appendChild(grid);
+            grid.addEventListener('mouseover', () => {
+                grid.classList.add('hover');
+            })
+            grid.addEventListener('mouseout', () => {
+                grid.classList.remove('hover');
+            })
         }
     }
-    
 }
 
-function removeGrid(oldUserChoice) {
+function removeGrid() {
     const subCanvas = document.querySelector('.subCanvas');
-    for (let i = 0; i < oldUserChoice; i++) {
-        canvas.remove(subCanvas);
-        const grid = document.querySelector('.blankItem');
-        for (let i = 0; i < oldUserChoice; i++) {
-            subCanvas.remove(grid);
-        }
-    }
+    canvas.removeChild(subCanvas);
 }
 
-// for (let i = 0; i < userChoice; i++) {
-//     const subCanvas = document.createElement('div');
-//     subCanvas.classList.add('subCanvas');
-//     canvas.appendChild(subCanvas);
-//     for (let i = 0; i < userChoice; i++) {
-//         const grid = document.createElement('div');
-//         grid.classList.add('blankItem');
-//         subCanvas.appendChild(grid);
-//         grid.addEventListener('mouseover', () => {
-//             grid.classList.add('hover');
-//         })
-//         grid.addEventListener('mouseout', () => {
-//             grid.classList.remove('hover');
-//         })
-//     }
-// }
-
-createGrid(userChoice);
+for (let i = 0; i < userChoice; i++) {
+    const subCanvas = document.createElement('div');
+    subCanvas.classList.add('subCanvas');
+    canvas.appendChild(subCanvas);
+    for (let i = 0; i < userChoice; i++) {
+        const grid = document.createElement('div');
+        grid.classList.add('blankItem');
+        subCanvas.appendChild(grid);
+        grid.addEventListener('mouseover', () => {
+            grid.classList.add('hover');
+        })
+        grid.addEventListener('mouseout', () => {
+            grid.classList.remove('hover');
+        })
+    }
+}
